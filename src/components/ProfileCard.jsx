77ebdcf33ab42cca660b95e3b57df078851478e9
Joe O <j0liverko@gmail.com>
2020-07-25 20:04:42 -0500

    added jsx for profile card

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ProfileCard = () => {
+
+    return(
+        <div>
+            <section>
+                <h3></h3>
+                <div>
+                    <img src="" alt=""/>
+                </div>
+                <div>
+                    <h6>view my</h6>
+                        <ul>
+                            <li>resume</li>
+                            <li>projects</li>
+                        </ul>
+                </div>
+            </section>
+
+            <section>
+                <ul>
+                    <li>fake age</li>
+                    <li>location</li>
+                    <li></li>
+                </ul>
+            </section>
+        </div>
+    )
+}
